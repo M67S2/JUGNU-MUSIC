bec923ab0f8f03bbdd30043859cea90e249e57b6
M67S2 <96144512+M67S2@users.noreply.github.com>
2022-09-29 19:25:04 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,7 @@ const client = require("../index");
 client.on("ready", async () => {
   console.log(`${client.user.username} Is Online`);
   client.user.setActivity({
-    name: `@${client.user.username} /hi`,
+    name: `hi`,
     type: ActivityType.Playing,
   });
 
